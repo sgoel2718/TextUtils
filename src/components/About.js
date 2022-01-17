c080a384d49f6837298bc8ba20cb66feb6faf85e
sgoel2718 <sgoel2718@gmail.com>
2022-01-17 09:07:40 +0530

    Some minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -40,36 +40,36 @@ export default function About(props) {
         <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
             <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
+                <strong>Analyze Your Text</strong>
             </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                TextUtils gives you a way to analyze your text quickly and efficiently.
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header" id="headingTwo">
             <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
+                <strong>Free to Use</strong>
             </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                TextUtils is a free character and word counter tool and able to chage lowercase to Uppercase and vice-versa. It will copy the text, remove extra space and preview the text and has a dark and light mode facility also.
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header" id="headingThree">
             <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
+                <strong>Browser Comatible</strong>
             </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                This word and character counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, etc.
             </div>
             </div>
         </div>
